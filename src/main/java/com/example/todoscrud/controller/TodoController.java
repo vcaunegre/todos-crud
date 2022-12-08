e20e26663222e3342b912a44f2862b8951fe0a5c
vcaunegre <vincent.caunegre@gmail.com>
2022-12-08 20:16:11 +0100

    cross origin

--- src/main/java/com/example/todoscrud/controller/TodoController.java
+++ src/main/java/com/example/todoscrud/controller/TodoController.java
@@ -3,6 +3,7 @@ package com.example.todoscrud.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.example.todoscrud.model.Todo;
 import com.example.todoscrud.service.TodoService;
 
 @RestController
+@CrossOrigin(origins = { "http://localhost:4200", "https://ngtodolist64.netlify.app" })
 @RequestMapping(path = "todos")
 public class TodoController {
 
