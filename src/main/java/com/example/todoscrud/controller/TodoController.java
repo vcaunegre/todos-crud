6727267ed1aa00f27dce77252a823a0a8b7023bc
vcaunegre <vincent.caunegre@gmail.com>
2022-12-10 00:43:22 +0100

    update version

--- src/main/java/com/example/todoscrud/controller/TodoController.java
+++ src/main/java/com/example/todoscrud/controller/TodoController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.todoscrud.model.Todo;
 import com.example.todoscrud.service.TodoService;
 
